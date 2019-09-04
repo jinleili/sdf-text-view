a5d1ca78391215e0c9010ae0e35f045020a26fb2
li jinlei <grenlight@icloud.com>
2019-09-04 12:44:00 +0800

    Explain screen flickering reason

--- examples/demo/main.rs
+++ examples/demo/main.rs
@@ -24,7 +24,7 @@ fn main() {
     let v = AppView::new(window);
 
     let mut surface_view = idroid::SDFTextView::new(v);
-    surface_view.bundle_image("math0.png".to_string());
+    surface_view.bundle_image("math3.png".to_string());
     // winit 0.20.0-alpha3 不会主动触发 WindowEvent::Resized 事件了
 
     events_loop.run(move |event, _, control_flow| {
